383c0770169fe3eb4365bb1c3fe8fb72324332b0
zanpatryk <patryk.zan@wp.pl>
2026-04-01 16:10:57 +0200

    fix: resolve yaml profile conflict and mock databases for context test

--- src/test/java/org/example/spendingtracker/SpendingTrackerApplicationTests.java
+++ src/test/java/org/example/spendingtracker/SpendingTrackerApplicationTests.java
@@ -1,10 +1,19 @@
 package org.example.spendingtracker;
 
+import com.rabbitmq.client.ConnectionFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class SpendingTrackerApplicationTests {
+
+    @MockitoBean
+    private MongoTemplate mongoTemplate;
+
+    @MockitoBean
+    private ConnectionFactory connectionFactory;
 
     @Test
     void contextLoads() {
