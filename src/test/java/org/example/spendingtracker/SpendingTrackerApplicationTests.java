d559d0fee174cba0d4581f7e2b537ed635896694
zanpatryk <patryk.zan@wp.pl>
2026-04-01 16:20:41 +0200

    fix: simplify yaml config and isolate test profile

--- src/test/java/org/example/spendingtracker/SpendingTrackerApplicationTests.java
+++ src/test/java/org/example/spendingtracker/SpendingTrackerApplicationTests.java
@@ -4,9 +4,11 @@ import com.rabbitmq.client.ConnectionFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SpendingTrackerApplicationTests {
 
     @MockitoBean
